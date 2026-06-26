1ea0505ed27eafd4bfce2f124230d0f2ddbf9f56
Jakub Szydełko <kubarroo248@interia.pl>
2026-06-27 00:16:31 +0200

    Add simple window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
+#include "SDL3/SDL_events.h"
+#include "SDL3/SDL_init.h"
+#include "SDL3/SDL_video.h"
 #include <SDL3/SDL.h>
-#include <iostream>
 
 int main() {
-    std::cout << "Hello World!\n";
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window{SDL_CreateWindow("Snake", 800, 640, SDL_WINDOW_RESIZABLE)};
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }

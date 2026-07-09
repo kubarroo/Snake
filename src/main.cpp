6aa10374918003e4b4b34f40cb70d7e3b28a5e04
Jakub Szydełko <kubarroo248@interia.pl>
2026-07-09 03:38:09 +0200

    add renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,37 @@
+#include "SDL3/SDL_error.h"
 #include "SDL3/SDL_events.h"
 #include "SDL3/SDL_init.h"
+#include "SDL3/SDL_log.h"
+#include "SDL3/SDL_pixels.h"
+#include "SDL3/SDL_render.h"
 #include "SDL3/SDL_video.h"
 #include <SDL3/SDL.h>
 
-int main() {
-    SDL_Init(SDL_INIT_VIDEO);
+struct GameCTX {
+    SDL_Renderer *renderer = nullptr;
+    SDL_Window *window = nullptr;
+};
 
-    SDL_Window *window{SDL_CreateWindow("Snake", 800, 640, SDL_WINDOW_RESIZABLE)};
+void DrawFrame(GameCTX &ctx) {
+    SDL_SetRenderDrawColorFloat(ctx.renderer, 1.0f, 1.0f, 1.0f, SDL_ALPHA_OPAQUE_FLOAT);
+
+    SDL_RenderClear(ctx.renderer);
+    SDL_RenderPresent(ctx.renderer);
+}
+
+int main() {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    GameCTX ctx;
+
+    if (!SDL_CreateWindowAndRenderer("Snake", 1920, 1080, SDL_WINDOW_RESIZABLE, &ctx.window,
+                                    &ctx.renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return 1;
+    }
 
     bool isRunning = true;
     SDL_Event event;
@@ -17,9 +42,10 @@ int main() {
                 isRunning = false;
             }
         }
+        DrawFrame(ctx);
     }
 
-    SDL_DestroyWindow(window);
+    SDL_DestroyWindow(ctx.window);
     SDL_Quit();
 
     return 0;
